965da11c16249d723fd3451b9f1cf40ba8c4546c
Marius Ilie <mariusjcb@Mariuss-iMac.local>
2017-07-08 00:36:17 +0300

    Initial Commit

---
+++ Heimdall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heimdall
+//
+//  Created by Marius Ilie on 08/07/2017.
+//  Copyright © 2017 Marius Ilie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
